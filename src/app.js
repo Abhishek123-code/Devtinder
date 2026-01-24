d123eade14f34775352af810fba7bf9018f34896
Abhishek Ojha <aojha6159@gmail.com>
2026-01-24 21:50:08 +0530

    Tested GET,POST,DELETE on Postman

--- src/app.js
+++ src/app.js
@@ -4,22 +4,30 @@ const app = express();
 
 //currenty using node --watch install nodemon if necessary
 
+app.get("/user", (req, res) => {
+  res.send({ firstname: "Abhishek", lastname: "Ojha" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("data posted successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("data patched successfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send({ firstname: "kuchu", lastname: "puchu" });
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully");
+});
+
 app.use("/test", (req, res) => {
   res.send("This is a test route");
 });
 
-app.use("/hello", (req, res) => {
-  res.send("HELLO hello hello");
-});
-
-app.use("/namaste", (req, res) => {
-  res.send("Namaste hgkhslh");
-});
-
-app.use((req, res) => {
-  res.send("This is a express server");
-  // If we write this app.use at top then it will be executed for every route
-});
 app.listen(7777, () => {
   console.log("successfully connected to the server");
 });
