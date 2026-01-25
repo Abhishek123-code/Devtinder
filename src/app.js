b05765b76fe43c1c4bab62d7a0c01eea276ba19b
Abhishek Ojha <aojha6159@gmail.com>
2026-01-25 13:53:05 +0530

    tested query and param in route

--- src/app.js
+++ src/app.js
@@ -3,10 +3,39 @@ import express from "express";
 const app = express();
 
 //currenty using node --watch install nodemon if necessary
-
+// app.use("/user", (req, res) => {
+//   res.send("LOL");
+// });
 app.get("/user", (req, res) => {
+  //http://localhost:7777/user?userid=101&password=secret
+  console.log(req.query); //Object: null prototype] { userid: '101', password: 'secret' }
+  console.log({ ...req.query }); //{ userid: '101', password: 'secret' }
   res.send({ firstname: "Abhishek", lastname: "Ojha" });
 });
+
+// app.get("/user/:userid/name/:password", (req, res) => {
+//   console.log(req.params); //[Object: null prototype] { userid: 'fly' }
+//   console.log({...req.params});
+//   res.send({ firstname: "Abhishek", lastname: "Ojha" });
+// });
+
+// app.get(/.*fly$/, (req, res) => {
+//   res.send({ firstname: "Abhishek", lastname: "Ojha" });
+// });
+
+// app.get(/.*fly$/, (req, res) => {
+//   res.send({ firstname: "Abhishek", lastname: "Ojha" });
+// });
+
+//can write anythhing between us and er
+// app.get("/us*er", (req, res) => {
+//   res.send({ firstname: "Abhishek", lastname: "Ojha" });
+// });
+
+//regex
+// app.get(/s/, (req, res) => {
+//   res.send({ firstname: "Abhishek", lastname: "Ojha" });
+// });
 
 app.post("/user", (req, res) => {
   res.send("data posted successfully");
