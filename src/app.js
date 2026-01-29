0d9bb093b973f16eedce3862ac5e1e452d7bcd44
Abhishek Ojha <aojha6159@gmail.com>
2026-01-29 19:29:33 +0530

    error-handling

--- src/app.js
+++ src/app.js
@@ -3,22 +3,41 @@ import { adminAuth, userAuth } from "./middlewares/auth.js";
 
 const app = express();
 
+app.get(
+  "/user",
+  (req, res) => {
+    // try{
+    throw new Error("eroor eroor");
+    res.send("data sent");
+  },
+  //   catch(err){
+  //     res.status(500).send("Something went wrong");
+  //   }
+  // }
+);
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    res.status(500).send("Error occurred");
+  }
+});
+
 //currenty using node --watch install nodemon if necessary
-app.use("/admin", adminAuth);
+// app.use("/admin", adminAuth);
 
-app.get("/user/login", (req, res) => {
-  res.send("User login successfully");
-});
+// app.get("/user/login", (req, res) => {
+//   res.send("User login successfully");
+// });
 
-app.get("/user/data", userAuth, (req, res) => {
-  res.send("Fetched user data");
-});
-app.get("/admin/getAllUser", (req, res) => {
-  res.send("All data fetched successfully");
-});
-app.get("/admin/deleteUser", (req, res) => {
-  res.send("User deleted successfully");
-});
+// app.get("/user/data", userAuth, (req, res) => {
+//   res.send("Fetched user data");
+// });
+// app.get("/admin/getAllUser", (req, res) => {
+//   res.send("All data fetched successfully");
+// });
+// app.get("/admin/deleteUser", (req, res) => {
+//   res.send("User deleted successfully");
+// });
 
 // app.get("/user", (req, res) => {
 //   http://localhost:7777/user?userid=101&password=secret
@@ -47,25 +66,25 @@ app.get("/admin/deleteUser", (req, res) => {
 //   res.send({ firstname: "Abhishek", lastname: "Ojha" });
 // });
 
-app.post("/user", (req, res) => {
-  res.send("data posted successfully");
-});
+// app.post("/user", (req, res) => {
+//   res.send("data posted successfully");
+// });
 
-app.patch("/user", (req, res) => {
-  res.send("data patched successfully");
-});
+// app.patch("/user", (req, res) => {
+//   res.send("data patched successfully");
+// });
 
-app.put("/user", (req, res) => {
-  res.send({ firstname: "kuchu", lastname: "puchu" });
-});
+// app.put("/user", (req, res) => {
+//   res.send({ firstname: "kuchu", lastname: "puchu" });
+// });
 
-app.delete("/user", (req, res) => {
-  res.send("Data deleted successfully");
-});
+// app.delete("/user", (req, res) => {
+//   res.send("Data deleted successfully");
+// });
 
-app.use("/test", (req, res) => {
-  res.send("This is a test route");
-});
+// app.use("/test", (req, res) => {
+//   res.send("This is a test route");
+// });
 
 app.listen(7777, () => {
   console.log("successfully connected to the server");
